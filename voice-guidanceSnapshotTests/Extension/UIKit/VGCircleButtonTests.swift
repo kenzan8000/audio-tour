4f7a365fb62b45ba573ce76974ae139de608da83
kenzan8000 <me@kenzan8000.org>
2021-04-14 22:47:44 +0900

    add VGCircleButton snapshot testing

---
+++ voice-guidanceSnapshotTests/Extension/UIKit/VGCircleButtonTests.swift
@@ -0,0 +1,32 @@
+import SnapshotTesting
+import XCTest
+@testable import voice_guidance
+
+// MARK: - VGCircleButtonTests
+class VGCircleButtonTests: XCTestCase {
+
+  // MARK: life cycle
+
+  override func setUpWithError() throws {
+    super.setUp()
+  }
+
+  override func tearDownWithError() throws {
+    super.tearDown()
+  }
+  
+  // MARK: test
+  
+  func testVGCircleButton_whenInitialState_snapshotTest() throws {
+    let sut = VGCircleButton(image: UIImage(systemName: "location.fill"))
+    
+    [(UIUserInterfaceStyle.dark, "dark"), (UIUserInterfaceStyle.light, "light")].forEach { style, named in
+      sut.overrideUserInterfaceStyle = style
+      assertSnapshot(
+        matching: sut,
+        as: .image(precision: 0.98),
+        named: named
+      )
+    }
+  }
+}
