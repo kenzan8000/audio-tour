ad280ece2fc5455751b9a7bae0d9caef0a5a498f
kenzan8000 <me@kenzan8000.org>
2021-04-16 22:05:19 +0900

    add VGSideMenuTableViewCell snapshot test

---
+++ voice-guidanceSnapshotTests/Main/SideMenu/VGSideMenuTableViewCellTests.swift
@@ -0,0 +1,32 @@
+import SnapshotTesting
+import XCTest
+@testable import voice_guidance
+
+// MARK: - VGSideMenuTableViewCellTests
+class VGSideMenuTableViewCellTests: XCTestCase {
+
+  // MARK: life cycle
+
+  override func setUpWithError() throws {
+    super.setUp()
+  }
+
+  override func tearDownWithError() throws {
+    super.tearDown()
+  }
+  
+  // MARK: test
+  
+  func testVGSideMenuTableViewCell_whenInitialState_snapshotTest() throws {
+    let sut = VGSideMenuTableViewCell(title: "Tutorial")
+    
+    [(UIUserInterfaceStyle.dark, "dark"), (UIUserInterfaceStyle.light, "light")].forEach { style, named in
+      sut.overrideUserInterfaceStyle = style
+      assertSnapshot(
+        matching: sut,
+        as: .image(precision: 0.98, traits: .iPhoneX(.portrait)),
+        named: named
+      )
+    }
+  }
+}
