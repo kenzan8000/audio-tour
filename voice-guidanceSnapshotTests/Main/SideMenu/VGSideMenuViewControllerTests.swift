662f422eaf4ff434a083a8aa6f0923b1374bdba5
kenzan8000 <me@kenzan8000.org>
2021-04-16 22:08:43 +0900

    add VGSideMenuViewController snapshot test

---
+++ voice-guidanceSnapshotTests/Main/SideMenu/VGSideMenuViewControllerTests.swift
@@ -0,0 +1,32 @@
+import SnapshotTesting
+import XCTest
+@testable import voice_guidance
+
+// MARK: - VGSideMenuViewControllerTests
+class VGSideMenuViewControllerTests: XCTestCase {
+
+  // MARK: life cycle
+
+  override func setUpWithError() throws {
+    super.setUp()
+  }
+
+  override func tearDownWithError() throws {
+    super.tearDown()
+  }
+  
+  // MARK: test
+  
+  func testVGSideMenuViewController_whenInitialState_snapshotTest() throws {
+    let sut = VGSideMenuViewController(viewModel: VGSideMenuViewModel(sideMenus: [.init(title: "Tutorial"), .init(title: "AR Tutorial")]))
+    
+    [(UIUserInterfaceStyle.dark, "dark"), (UIUserInterfaceStyle.light, "light")].forEach { style, named in
+      sut.overrideUserInterfaceStyle = style
+      assertSnapshot(
+        matching: sut,
+        as: .image(on: .iPhoneX, precision: 0.98, traits: .iPhoneX(.portrait)),
+        named: named
+      )
+    }
+  }
+}
