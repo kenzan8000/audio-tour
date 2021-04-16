052bc185926e8f1fa7b4267352f2a5aabf186576
kenzan8000 <me@kenzan8000.org>
2021-04-16 22:29:05 +0900

    add VGSearchView snapshot test

---
+++ voice-guidanceSnapshotTests/Main/Search/VGSearchViewTests.swift
@@ -0,0 +1,51 @@
+import SnapshotTesting
+import XCTest
+@testable import voice_guidance
+
+// MARK: - VGSearchViewTests
+class VGSearchViewTests: XCTestCase {
+
+  // MARK: property
+  
+  var sut: VGSearchView!
+  var window: UIWindow!
+  
+  // MARK: life cycle
+
+  override func setUpWithError() throws {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    sut = VGSearchView(on: window)
+    super.setUp()
+  }
+
+  override func tearDownWithError() throws {
+    sut = nil
+    window = nil
+    super.tearDown()
+  }
+  
+  // MARK: test
+  
+  func testVGSearchView_whenInitialState_snapshotTest() throws {
+    [(UIUserInterfaceStyle.dark, "dark"), (UIUserInterfaceStyle.light, "light")].forEach { style, named in
+      sut.overrideUserInterfaceStyle = style
+      assertSnapshot(
+        matching: sut,
+        as: .image(precision: 0.98, traits: .iPhoneX(.portrait)),
+        named: named
+      )
+    }
+  }
+  
+  func testVGSearchView_whenTextEqualsToGoldenGateBridge_snapshotTest() throws {
+    sut.textField.text = "The Golden Gate Bridge"
+    [(UIUserInterfaceStyle.dark, "dark"), (UIUserInterfaceStyle.light, "light")].forEach { style, named in
+      sut.overrideUserInterfaceStyle = style
+      assertSnapshot(
+        matching: sut,
+        as: .image(precision: 0.98, traits: .iPhoneX(.portrait)),
+        named: named
+      )
+    }
+  }
+}
