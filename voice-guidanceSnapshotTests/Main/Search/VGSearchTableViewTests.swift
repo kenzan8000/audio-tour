5b8b35edd1f60acaeca0118bc0e35d686e8d60d7
kenzan8000 <me@kenzan8000.org>
2021-04-16 22:20:14 +0900

    add VGSearchTableView snapshot test

---
+++ voice-guidanceSnapshotTests/Main/Search/VGSearchTableViewTests.swift
@@ -0,0 +1,32 @@
+import SnapshotTesting
+import XCTest
+@testable import voice_guidance
+
+// MARK: - VGSearchTableViewTests
+class VGSearchTableViewTests: XCTestCase {
+
+  // MARK: life cycle
+
+  override func setUpWithError() throws {
+    super.setUp()
+  }
+
+  override func tearDownWithError() throws {
+    super.tearDown()
+  }
+  
+  // MARK: test
+  
+  func testVGSearchTableView_whenInitialState_snapshotTest() throws {
+    let sut = VGSearchTableView()
+    
+    [(UIUserInterfaceStyle.dark, "dark"), (UIUserInterfaceStyle.light, "light")].forEach { style, named in
+      sut.overrideUserInterfaceStyle = style
+      assertSnapshot(
+        matching: sut,
+        as: .image(precision: 0.98, traits: .iPhoneX(.portrait)),
+        named: named
+      )
+    }
+  }
+}
