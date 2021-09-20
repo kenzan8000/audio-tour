ef390443850b25a9dac79022d3ca084b69e190a1
kenzan8000 <me@kenzan8000.org>
2021-09-20 23:25:36 +0900

    add VGTutorialViewController ui test

--- voice-guidanceUITests/Tutorial/VGTutorialViewControllerTests.swift
+++ voice-guidanceUITests/Tutorial/VGTutorialViewControllerTests.swift
@@ -14,9 +14,24 @@ class VGTutorialViewControllerTests: XCTestCase {
 
   // MARK: test
   
-  func test() throws {
+  func testVGTutorialViewController_whenInitialState_uiTest() throws {
     let app = XCUIApplication()
+    let permission = XCUIApplication(bundleIdentifier: "com.apple.springboard")
     app.launchArguments += ["-VGUserDefaults", "VGUserDefaults.doneTutorial", "Bool", "false"]
     app.launch()
+    
+    app.buttons["Next"].tap()
+    
+    app.buttons["Next"].tap()
+    if permission.alerts["Allow “Audio Tour” to use your location?"].waitForExistence(timeout: 1.0) {
+      permission.alerts["Allow “Audio Tour” to use your location?"].buttons["Allow While Using App"].tap()
+    }
+    
+    app.buttons["Next"].tap()
+    if permission.alerts["“Audio Tour” Would Like to Access the Camera"].waitForExistence(timeout: 1.0) {
+      permission.alerts["“Audio Tour” Would Like to Access the Camera"].buttons["OK"].tap()
+    }
+    
+    app.buttons["Done"].tap()
   }
 }
