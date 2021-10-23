969a414b97bcabe5a6e268d7bc8a55e2cebccbff
kenzan8000 <me@kenzan8000.org>
2021-10-24 00:04:32 +0900

    remove voice-guidanceTests copy-Info.plist and update VGTestingAppDelegate

--- Tests/VGTestingAppDelegate.swift
+++ Tests/VGTestingAppDelegate.swift
@@ -6,7 +6,13 @@ class VGTestingAppDelegate: UIResponder, UIApplicationDelegate {
 func application(
   _ application: UIApplication,
   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    true
+    for sceneSession in application.openSessions {
+      application.perform(Selector(("_removeSessionFromSessionSet:")), with: sceneSession)
+      for sceneSession in application.openSessions {
+        application.perform(Selector(("_removeSessionFromSessionSet:")), with: sceneSession)
+      }
+    }
+    return true
   }
 
   func application(
