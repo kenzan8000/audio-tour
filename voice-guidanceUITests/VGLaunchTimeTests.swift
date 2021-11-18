a66d0e7d4e404c9a56ba210e1fcead68039cbb83
kenzan8000 <me@kenzan8000.org>
2021-11-18 22:18:55 +0900

    add app launch time performance test

---
+++ voice-guidanceUITests/VGLaunchTimeTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+
+final class VGLaunchTimeTests: XCTestCase {
+
+  // MARK: life cycle
+  
+  override func setUpWithError() throws {
+    continueAfterFailure = false
+  }
+
+  override func tearDownWithError() throws {
+  }
+
+  // MARK: test
+/*
+  func test_whenInitialState_measureLaunchTime() throws {
+    measure(metrics: [XCTApplicationLaunchMetric(waitUntilResponsive: true)]) {
+      XCUIApplication().launch()
+    }
+  }
+*/
+}
